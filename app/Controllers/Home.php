f452da70a6c9c35a21a3d69f3c3d8c641e9a483f
unknown <mikipary@gmail.com>
2024-05-02 20:20:12 +0100

    Dashboard update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,24 +8,13 @@ class Home extends Controller
 	
 	public function index()
 	{
-		// $user = $this->load_model("user"); 
-		// $arr = ['gender' => 'male', 'firstname' => "Kareem", 'lastname' => "Jerrymiah", 'user_id' => "24fgddhcbdjd"];
-		// $data = ['male', 'firstname', "Jerrymiah", "24fgddhcbdjd"];
 
 		if($check = !Auth::logged_in()) {
 			$this->redirect('login');
 		}
-		$user = new User();
 
-		// $data = $user->where('firstname','Jane');
-		// $data = $user->insert($arr);
-		// $data = $user->update(1,$arr);
-		// $data = $user->delete(4);
-		$data = $user->findAll();
 
-		$this->load_view('home',[
-			'rows' => $data,
-		]);
+		$this->load_view('home');
 	}
 
 
--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -1,8 +1,16 @@
 <?php $this->load_view('includes/header'); ?>
 <?php $this->load_view('includes/nav'); ?>
 
-		<div class="container-fluid">
-			
+		<div class="container-fluid p-4 shadow mx-auto" style="max-width: 1000px">
+			<div class="row">
+				<div class="col-3 shadow rounded m-4 border card p-0">
+					<div class="card-header">SCHOOLS</div>
+					<h1 class="text-center" style="font-size: 80px;">
+						<i class="fa fa-graduation-cap"></i>
+					</h1>
+					<div class="card-footer">View all schools</div>
+				</div>
+			</div>
 		</div>
 
 <?php $this->load_view('includes/footer'); ?>
